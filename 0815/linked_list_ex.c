6335f4ed8f58f806cfdcfb2d8a17e41a87d5fd76
juhylee <juhylee@student.42seoul.kr>
2023-08-15 18:04:50 +0900

    linked_list_ex.c

---
+++ 0815/linked_list_ex.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+typedef struct st_list_node
+{
+	int item;
+	struct st_list_node* next;
+	struct st_list_node* prev;
+} list_node_t;
+
+void insert_front(list_node_t** head, int item)
+{
+	list_node_t* new = (list_node_t*)malloc(sizeof(list_node_t));
+	new->item = item;
+	new->prev = NULL;
+	new->next = *head;
+	*head = new;
+	new->next->prev = new;
+	return;
+}
+
+int main(void)
+{
+	list_node_t* head = NULL;
+	insert_front(&head, 42);
+	insert_front(&head, 33);
+}
